1d79dba69432e450bd17538ceeb6bcc2cafcf060
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-07-27 10:58:18 +0300

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,127 @@
-from django.db import models
 
+from django.db import models
+import datetime as dt
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.forms import ModelForm, widgets
 # Create your models here.
+
+class Hobbies(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name= models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name=models.CharField(max_length=250)
+    title=models.CharField(max_length=250)
+    description = models.TextField()
+    image=CloudinaryField('image')
+    hobbies = models.ForeignKey(Hobbies, on_delete=models.CASCADE)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+    
+class Projects(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    image = CloudinaryField("image")
+    url = models.URLField(blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    @classmethod
+    def search_by_title(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        project = cls.objects.get(id=id)
+        return project
+
+    @classmethod
+    def get_all_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def get_all_projects_by_user(cls, user):
+        projects = cls.objects.filter(user=user)
+        return projects
+
+    
+    def update_project(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    image = CloudinaryField("image")
+    content = models.TextField()
+
+    url = models.URLField(blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def save_blog(self):
+        self.save()
+
+    def delete_blog(self):
+        self.delete()
+
+    @classmethod
+    def filter_by_id(cls, id):
+        blog= Blog.objects.filter(id=id).title()
+        return blog
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment=models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.comment)
+
+class Like(models.Model):
+    comment=models.OneToOneField(Comment,related_name ='likes' ,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.comment.comment)
+
+class DisLike(models.Model):
+    comment = models.OneToOneField(Comment, related_name="dis_likes", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.comment.comment)
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -77,10 +80,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jay',
-        
+        'NAME': 'portfolio',
+        'USER':'jay',
+        'PASSWORD':'JAY',
     }
 }
+
 
 
 # Password validation
@@ -123,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config( 
+  cloud_name = "jeddy", 
+  api_key = "136262568691725", 
+  api_secret = "D8mX4Rvxz6VyGLhZDIprEG26Q60" 
+)
