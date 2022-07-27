8fc8e935b8f4633964d7f15e307ba693c3fbb78c
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-07-27 16:56:38 +0300

    configure serializers

--- app/views.py
+++ app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render,redirect
 # Create your views here.
 def home (request):
     return render(request, 'home.html',{})
+
+
+class ProfileList(APIView):
+    permission_classes =(IsAdminOrReadOnly)
+    def get (self,request,format=None ):
+        all_projects
